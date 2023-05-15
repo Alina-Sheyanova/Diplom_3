33fe1bdfffdde972b3b72436a93e1fbe451436cc
Alina Sheyanova <sheyanova@proitr.ru>
2023-05-15 15:39:17 +0300

    fixed NavigatingSectionsConstructorTest

--- src/test/java/uitest/NavigatingSectionsConstructorTest.java
+++ src/test/java/uitest/NavigatingSectionsConstructorTest.java
@@ -1,26 +1,31 @@
 package uitest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.openqa.selenium.By;
 import pageobjects.MainPage;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class NavigatingSectionsConstructorTest extends BaseTest{
+
     @Test
-    public void testEqualsTextQuestionsAboutImportant(){
+    public void checkBreadSection(){
         MainPage mainPage = new MainPage(driver);
-        mainPage.pushToppingSection();
-        assertTrue("Выбрана вкладка начинок", mainPage.selectToppingSectionButton());
+        mainPage.pushSauceSection();
+        mainPage.pushBreadSection();
+        assertTrue("Выбрана вкладка булок", mainPage.selectBreadSectionButton());
+    }
+    @Test
+    public void checkSauceSection(){
+        MainPage mainPage = new MainPage(driver);
+        mainPage.pushSauceSection();
+        assertTrue("Выбрана вкладка соусов", mainPage.selectSauceSectionButton());
     }
 
     @Test
-    public void checkBunsPass(){
+    public void checkToppingSection(){
         MainPage mainPage = new MainPage(driver);
-        mainPage.pushBreadSection();
-        assertTrue("Выбрана вкладка булок", mainPage.selectBreadSectionButton());
+        mainPage.pushToppingSection();
+        assertTrue("Выбрана вкладка начинок", mainPage.selectToppingSectionButton());
     }
 
 
