c01cf82bade49dc662ae994fbd8bf5a3a4702980
Alina Sheyanova <sheyanova@proitr.ru>
2023-05-24 22:28:09 +0300

    add info about user

--- src/main/java/constans/ApiEndpoints.java
+++ src/main/java/constans/ApiEndpoints.java
@@ -1,15 +1,13 @@
 package constans;
 public class ApiEndpoints {
     public static String BASE_URL = "https://stellarburgers.nomoreparties.site/api";
-    public static String USER = "/auth/user";
     public static String CREATE_USER = "/auth/register";
     public static String LOGIN_USER = "auth/login";
     public static String LOGOUT_USER = "/auth/logout";
     public static String PASSWORD_RESET = "/password-reset";
-    public static String INGREDIENTS_DATA = "/ingredients";
-    public static String CREATE_ORDER = "/orders";
-    public static String ALL_ORDERS = "/orders/all";
-    public static String UPDATE_USER_AUTH_TOKEN = "/auth/token";
+    public final static String API_REGISTER = "api/auth/register";
+    public final static String API_LOGIN = "api/auth/login";
+    public final static String API_USER = "/api/auth/user";
     public final static String YANDEX_DRIVER = "src/main/resources/yandexdriver.exe";
 
 }
